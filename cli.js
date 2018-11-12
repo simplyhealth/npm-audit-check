016a7d2af5f3ec08715775ff4780b66ad3f8427a
Chris Chalke <chris.chalke@simplyhealth.co.uk>
2018-11-12 14:17:35 +0000

    Add colour and a new --test option

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
 const { spawn } = require('child_process')
+const chalk = require('chalk')
 
 program
   .version('1.0.0')
@@ -9,6 +10,7 @@ program
   .option('-m, --moderate <moderate policy>', 'Set policy for moderate vulnerabilities', parseInt)
   .option('-h, --high <high policy>', 'Set policy for high vulnerabilities', parseInt)
   .option('-c, --critical <critical policy>', 'Set policy for critical vulnerabilities', parseInt)
+  .option('-t, --test', 'Report on level of vulnerabilities but return a success exit code')
   .parse(process.argv);
 
 let audit = spawn('npm', ['audit', '--json'])
@@ -29,32 +31,35 @@ audit.on('close', (code) => {
   let vulnerable = false
 
   if (program.info !== undefined && output.metadata.vulnerabilities.info > program.info) {
-    console.log(`There are ${output.metadata.vulnerabilities.info} info vulnerabilities which is more than your allowed policy of ${program.info} `)
+    console.log(`There are ${output.metadata.vulnerabilities.info} info vulnerabilities which is more than your allowed policy of ${program.info}`)
     vulnerable = true
   }
 
   if (program.low !== undefined && output.metadata.vulnerabilities.low > program.low) {
-    console.log(`There are ${output.metadata.vulnerabilities.low} low vulnerabilities which is more than your allowed policy of ${program.low} `)
+    console.log(`There are ${output.metadata.vulnerabilities.low} low vulnerabilities which is more than your allowed policy of ${program.low}`)
     vulnerable = true
   }
 
   if (program.moderate !== undefined && output.metadata.vulnerabilities.moderate > program.moderate) {
-    console.log(`There are ${output.metadata.vulnerabilities.moderate} moderate vulnerabilities which is more than your allowed policy of ${program.moderate} `)
+    console.log(`There are ${chalk.yellow(output.metadata.vulnerabilities.moderate)} moderate vulnerabilities which is more than your allowed policy of ${chalk.yellow(program.moderate)}`)
     vulnerable = true
   }
 
   if (program.high !== undefined && output.metadata.vulnerabilities.high > program.high) {
-    console.log(`There are ${output.metadata.vulnerabilities.high} high vulnerabilities which is more than your allowed policy of ${program.high} `)
+    console.log(`There are ${chalk.red(output.metadata.vulnerabilities.high)} high vulnerabilities which is more than your allowed policy of ${chalk.red(program.high)}`)
     vulnerable = true
   }
 
   if (program.critical !== undefined && output.metadata.vulnerabilities.critical > program.critical) {
-    console.log(`There are ${output.metadata.vulnerabilities.critical} critical vulnerabilities which is more than your allowed policy of ${program.critical} `)
+    console.log(`There are ${chalk.magenta(output.metadata.vulnerabilities.critical)} critical vulnerabilities which is more than your allowed policy of ${chalk.magenta(program.critical)}`)
     vulnerable = true
   }
 
   if (vulnerable) {
     console.error("Run npm audit to get more details")
-    process.exit(1)
+    if (!program.test) {
+      console.log('Exiting with status code 1')
+      process.exit(1)
+    }
   }
 });
